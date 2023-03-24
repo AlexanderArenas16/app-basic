016cae1bf6e32300f0d5884940eada90a398e59a
Alexander Arenas <arenasocampoalexander@gmail.com>
2023-03-23 22:36:31 -0500

    add styles to login page

--- src/app/modules/login/login.module.ts
+++ src/app/modules/login/login.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LoginComponent } from './login.component';
-
-
+import { LoginComponent } from './pages/login.component';
+import { ButtonModule } from 'primeng/button';
+import { LoginRoutingModule } from './login-routing.module';
+import { PasswordModule } from 'primeng/password';
+import { InputTextModule } from 'primeng/inputtext';
 
 @NgModule({
   declarations: [
     LoginComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    LoginRoutingModule,
+    ButtonModule,
+    PasswordModule,
+    InputTextModule
   ]
 })
 export class LoginModule { }
